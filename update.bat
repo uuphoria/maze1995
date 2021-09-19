5bb9cd269a7e240d62108d0dce44068718e0f62c
Agent-11 <techgod18@outlook.com>
2021-09-19 17:33:28 -0400

    Deploy new updates

--- update.bat
+++ update.bat
@@ -1,9 +1,10 @@
 @echo off
+title Maze 95 JS update tool
 del /f index.html
 del /f index.css
 del /f maze95.js
 rmdir /Q /S textures
-rmdir /Q /S lib
+rmdir /Q /S game
 rmdir /Q /S levels
 rmdir /Q /S audio
 git clone https://github.com/maze95/maze95-js.git temp_clone
@@ -11,7 +12,7 @@ move temp_clone\index.html index.html
 move temp_clone\index.css index.css
 move temp_clone\maze95.js maze95.js
 move temp_clone\textures textures
-move temp_clone\lib lib
+move temp_clone\game game
 move temp_clone\levels levels
 move temp_clone\audio audio
 rmdir /Q /S temp_clone
