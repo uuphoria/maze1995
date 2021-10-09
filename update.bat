b538e116dd2c9ce646d081cc6866fb9690cdf24b
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-10-09 12:17:58 -0400

    Deploy Auto LF

--- update.bat
+++ update.bat
@@ -3,6 +3,7 @@ title Maze 95 JS update tool
 del /f index.html
 del /f index.css
 del /f maze95.js
+del /f .gitattributes
 rmdir /Q /S textures
 rmdir /Q /S game
 rmdir /Q /S levels
@@ -11,10 +12,10 @@ git clone https://github.com/maze95/maze95-js.git temp_clone
 move temp_clone\index.html index.html
 move temp_clone\index.css index.css
 move temp_clone\maze95.js maze95.js
+move temp_clone\.gitattributes .gitattributes
 move temp_clone\textures textures
 move temp_clone\game game
 move temp_clone\levels levels
 move temp_clone\audio audio
 rmdir /Q /S temp_clone
-echo Successfully updated Maze 95 JS.
 pause
