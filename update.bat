ffbeb6a99c40a361c310d19fcebacb7e50746429
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-10-12 15:52:44 -0400

    Deploy Collision creation tool!

--- update.bat
+++ update.bat
@@ -1,6 +1,7 @@
 @echo off
 title Maze 95 JS update tool
 del /f index.html
+del /f canvas-only.html
 del /f index.css
 del /f maze95.js
 del /f .gitattributes
@@ -10,6 +11,7 @@ rmdir /Q /S levels
 rmdir /Q /S audio
 git clone https://github.com/maze95/maze95-js.git temp_clone
 move temp_clone\index.html index.html
+move temp_clone\canvas-only.html canvas-only.html
 move temp_clone\index.css index.css
 move temp_clone\maze95.js maze95.js
 move temp_clone\.gitattributes .gitattributes
