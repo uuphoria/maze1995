394f0d0feb0d31f501cb0544bce949eb0528f257
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-10-14 16:06:41 -0400

    Deploy newest changes

--- textures/textures.js
+++ textures/textures.js
@@ -1,25 +1,28 @@
 import * as THREE from '../game/three.module.js'
 
-const wallTex = new THREE.TextureLoader().load("./textures/wall.png", function ( texture ) {
+let texPath = "./textures/"
+if (!window.location.href.includes("localhost") || !window.location.href.includes("127.0.0.1")) texPath = "https://maze95.js.org/textures/"
+
+const wallTex = new THREE.TextureLoader().load(`${texPath}wall.png`, function ( texture ) {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   texture.offset.set(0,-0.02)
-  texture.repeat.set(1,0.98)
+  texture.repeat.set(1,1.2)
 })
 wallTex.magFilter = THREE.NearestFilter
 export const wallMat = new THREE.MeshBasicMaterial({map: wallTex})
 
-const ceilingTex = new THREE.TextureLoader().load("./textures/ceiling.png", function ( texture ) {
+const ceilingTex = new THREE.TextureLoader().load(`${texPath}ceiling.png`, function ( texture ) {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   texture.offset.set(0,0)
-  texture.repeat.set(80,70)
+  texture.repeat.set(120,100)
 })
 ceilingTex.magFilter = THREE.NearestFilter
 export const ceilingMat = new THREE.MeshBasicMaterial({map: ceilingTex})
 
-const floorTex = new THREE.TextureLoader().load("./textures/floor.png", function ( texture ) {
+const floorTex = new THREE.TextureLoader().load(`${texPath}floor.png`, function ( texture ) {
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping
   texture.offset.set(0,0)
-  texture.repeat.set(30,30)
+  texture.repeat.set(50,50)
 })
 floorTex.magFilter = THREE.NearestFilter
 export const floorMat = new THREE.MeshBasicMaterial({map: floorTex})
--- update.bat
+++ update.bat
@@ -7,7 +7,6 @@ del /f maze95.js
 del /f .gitattributes
 rmdir /Q /S textures
 rmdir /Q /S game
-rmdir /Q /S levels
 rmdir /Q /S audio
 git clone https://github.com/maze95/maze95-js.git temp_clone
 move temp_clone\index.html index.html
@@ -17,7 +16,6 @@ move temp_clone\maze95.js maze95.js
 move temp_clone\.gitattributes .gitattributes
 move temp_clone\textures textures
 move temp_clone\game game
-move temp_clone\levels levels
 move temp_clone\audio audio
 rmdir /Q /S temp_clone
 pause
