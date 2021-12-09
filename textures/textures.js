b57f1c5eb356346b93c21ef4aa010740af100bd7
Agent-11 / Agent X <44549182+Agent-11@users.noreply.github.com>
2021-12-08 23:44:07 -0500

    PWA? - Un-nodeify

--- textures/textures.js
+++ textures/textures.js
@@ -1,1 +1,37 @@
-import*as THREE from"three";let texPath="./textures/";window.location.href.includes("localhost")&&window.location.href.includes("127.0.0.1")||(texPath="https://maze95.js.org/textures/");const wallTex=(new THREE.TextureLoader).load(`${texPath}wall.png`,(function(e){e.wrapS=e.wrapT=THREE.RepeatWrapping,e.offset.set(0,-.02),e.repeat.set(1,1.2)}));wallTex.magFilter=THREE.NearestFilter;export const wallMat=new THREE.MeshBasicMaterial({map:wallTex});const ceilingTex=(new THREE.TextureLoader).load(`${texPath}ceiling.png`,(function(e){e.wrapS=e.wrapT=THREE.RepeatWrapping,e.offset.set(0,0),e.repeat.set(120,100)}));ceilingTex.magFilter=THREE.NearestFilter;export const ceilingMat=new THREE.MeshBasicMaterial({map:ceilingTex});const floorTex=(new THREE.TextureLoader).load(`${texPath}floor.png`,(function(e){e.wrapS=e.wrapT=THREE.RepeatWrapping,e.offset.set(0,0),e.repeat.set(50,50)}));floorTex.magFilter=THREE.NearestFilter;export const floorMat=new THREE.MeshBasicMaterial({map:floorTex});const faceTexture=(new THREE.TextureLoader).load("./textures/fin.png");faceTexture.magFilter=THREE.NearestFilter;export const face=new THREE.MeshStandardMaterial({map:faceTexture,transparent:!0,opacity:.65});const startTexture=(new THREE.TextureLoader).load("./textures/start.png");startTexture.magFilter=THREE.NearestFilter;export const start=new THREE.MeshBasicMaterial({map:startTexture,transparent:!0,opacity:.6});
+import * as THREE from '../game/three.module.js'
+
+let texPath = "./textures/"
+if (!window.location.href.includes("localhost") || !window.location.href.includes("127.0.0.1")) texPath = "https://maze95.js.org/textures/"
+
+const wallTex = new THREE.TextureLoader().load(`${texPath}wall.png`, function ( texture ) {
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  texture.offset.set(0,-0.02)
+  texture.repeat.set(1,1.2)
+})
+wallTex.magFilter = THREE.NearestFilter
+export const wallMat = new THREE.MeshBasicMaterial({map: wallTex})
+
+const ceilingTex = new THREE.TextureLoader().load(`${texPath}ceiling.png`, function ( texture ) {
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  texture.offset.set(0,0)
+  texture.repeat.set(120,100)
+})
+ceilingTex.magFilter = THREE.NearestFilter
+export const ceilingMat = new THREE.MeshBasicMaterial({map: ceilingTex})
+
+const floorTex = new THREE.TextureLoader().load(`${texPath}floor.png`, function ( texture ) {
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  texture.offset.set(0,0)
+  texture.repeat.set(50,50)
+})
+floorTex.magFilter = THREE.NearestFilter
+export const floorMat = new THREE.MeshBasicMaterial({map: floorTex})
+
+
+const faceTexture = new THREE.TextureLoader().load("./textures/fin.png")
+faceTexture.magFilter = THREE.NearestFilter
+export const face = new THREE.MeshStandardMaterial({map: faceTexture, transparent: true, opacity: 0.65})
+
+const startTexture = new THREE.TextureLoader().load("./textures/start.png")
+startTexture.magFilter = THREE.NearestFilter
+export const start = new THREE.MeshBasicMaterial({map: startTexture, transparent: true, opacity: 0.6})
